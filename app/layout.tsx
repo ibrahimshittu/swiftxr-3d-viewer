97e279d0bea8749f8624d166d3fa039a24fc4df1
Ibrahim Shittu <ibshittu01@gmail.com>
2025-03-07 16:09:08 +0100

    fix: prevent body overflow

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${bigShouldersDisplay.className} antialiased`}>
+      <body
+        className={`${bigShouldersDisplay.className} antialiased overflow-hidden`}
+      >
         {children}
         <Toaster />
       </body>
