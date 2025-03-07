09349753484713e5c50ea0c41af4d03575ac9263
Ibrahim Shittu <ibshittu01@gmail.com>
2025-03-07 15:37:41 +0100

    Update README.md

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-console": "off",
+      "no-undef": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
